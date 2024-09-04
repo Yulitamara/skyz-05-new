7cbcd1ce2dc699edea8236f0c893aad9a5546d26
Yuli Tamara <79260463+Yulitamara@users.noreply.github.com>
2024-09-04 12:31:13 +0300

    s

--- up/js/sidebar.js
+++ up/js/sidebar.js
@@ -1,10 +1,12 @@
 const sidebar = document.querySelector(".side-nav");
 const sidebarBtn = document.getElementById("sideNavBtn");
 const navbarOverlay = document.querySelector(".navbar__overlay");
+const skyzName = document.querySelector(".skyz-name");
 
 sidebarBtn.addEventListener("click", () => {
   sidebar.classList.toggle("active");
   // navbarOverlay.classList.toggle("open");
+  skyzName.classList.toggle("active");
 });
 
 document.addEventListener("click", (event) => {
@@ -16,20 +18,23 @@ document.addEventListener("click", (event) => {
   }
 });
 
-// const acountsCollapse = document.getElementById("acountsCollapse");
-// const acountsBtn = document.querySelector(".btn-actions-menu");
+// window.addEventListener("resize", handleResize);
+// window.addEventListener("load", handleResize);
 
-// acountsBtn.addEventListener("click", () => {
-//   const sideBarItem = acountsBtn.closest(".side-nav__content--list__item");
+// function handleResize() {
+//   const navbarMain = document.querySelector(".navbar__main");
+//   const smallScreenContainer = document.querySelector(
+//     ".small-screen-append__navbar-main"
+//   );
 
-//   if (sideBarItem) {
-//     acountsCollapse.classList.toggle("active");
-//     if (acountsCollapse.classList.contains("active")) {
-//       acountsBtn.innerHTML = "expand_more";
-//       sideBarItem.style.background = "";
-//     } else {
-//       acountsBtn.innerHTML = "close";
-//       sideBarItem.style.background = "var(--color-permanent-bg)";
+//   if (window.innerWidth <= 985) {
+//     if (navbarMain && !smallScreenContainer.contains(navbarMain)) {
+//       smallScreenContainer.appendChild(navbarMain);
+//     }
+//   } else {
+//     const originalContainer = document.querySelector(".navbar__main-container"); // Replace with the original container
+//     if (navbarMain && !originalContainer.contains(navbarMain)) {
+//       originalContainer.appendChild(navbarMain);
 //     }
 //   }
-// });
+// }
