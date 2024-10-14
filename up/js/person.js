244c058a5ea7a1488d53a30e3345d9835c552fcf
Yuli Tamara <79260463+Yulitamara@users.noreply.github.com>
2024-10-14 09:56:47 +0300

    s

--- up/js/person.js
+++ up/js/person.js
@@ -584,29 +584,40 @@ document.getElementById("close-filters").addEventListener("click", function () {
 });
 
 
-  // Store the original content of elements
-  const originalContents = {};
+   // Store the original content of elements
+   const originalContents = {};
+   const originalTitleContents = {};
 
-  // Store the original contents for visibility-width elements
-  document.querySelectorAll('.visibility-width').forEach((element, index) => {
-      originalContents[index] = element.innerHTML;
-  });
+   // Store the original contents for visibility-width elements
+   document.querySelectorAll('.visibility-width').forEach((element, index) => {
+       originalContents[index] = element.innerHTML;
+   });
 
-  document.getElementById('visibility-btn').addEventListener('click', function() {
-      // Toggle button text
-      const btn = document.getElementById('visibility-btn');
-      btn.innerHTML = btn.innerHTML === 'visibility' ? 'visibility_off' : 'visibility';
+   // Store the original contents for visibility-title elements
+   document.querySelectorAll('.visibility-title').forEach((element, index) => {
+       originalTitleContents[index] = element.innerHTML;
+   });
 
-      // Toggle the content and class of elements with class 'visibility-width'
-      document.querySelectorAll('.visibility-width').forEach((element, index) => {
-          // Toggle between original content and empty string
-          element.innerHTML = element.innerHTML === '' ? originalContents[index] : '';
-          // Toggle the 'visibility-is-off' class
-          element.classList.toggle('visibility-is-off');
-      });
+   document.getElementById('visibility-btn').addEventListener('click', function() {
+       // Toggle button text
+       const btn = document.getElementById('visibility-btn');
+       btn.innerHTML = btn.innerHTML === 'visibility' ? 'visibility_off' : 'visibility';
 
-      // Toggle the class 'visibility-title-off' for elements with class 'visibility-width-title'
-      document.querySelectorAll('.visibility-width-title').forEach((element) => {
-          element.classList.toggle('visibility-title-off');
-      });
-  });
+       // Toggle the content and class of elements with class 'visibility-width'
+       document.querySelectorAll('.visibility-width').forEach((element, index) => {
+           // Toggle between original content and empty string
+           element.innerHTML = element.innerHTML === '' ? originalContents[index] : '';
+           // Toggle the 'visibility-is-off' class
+           element.classList.toggle('visibility-is-off');
+       });
+
+       // Toggle the class 'visibility-title-off' for elements with class 'visibility-width-title'
+       document.querySelectorAll('.visibility-width-title').forEach((element) => {
+           element.classList.toggle('visibility-title-off');
+       });
+
+       // Toggle the content of elements with class 'visibility-title'
+       document.querySelectorAll('.visibility-title').forEach((element, index) => {
+           element.innerHTML = element.innerHTML === '' ? originalTitleContents[index] : '';
+       });
+   });
